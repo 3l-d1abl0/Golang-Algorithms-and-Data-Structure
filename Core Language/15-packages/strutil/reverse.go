31e8752a37477c1aca6e50aee7cc20750350b9f0
sameer <sameer.barha12@gmail.com>
2022-05-29 17:15:42 +0530

    added example for go packages

---
+++ Core Language/15-packages/strutil/reverse.go
@@ -0,0 +1,12 @@
+package strutil
+
+//Starting with capital Letter - can be imported
+func Reverse(s string) string {
+
+	//int32
+	runes := []rune(s)
+	for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
+		runes[i], runes[j] = runes[j], runes[i]
+	}
+	return string(runes)
+}
